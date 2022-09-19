b0b1ef09b501bb77239c8bb962af66e1f8d9869b
Edmond <34758460+EdmondtheGreat@users.noreply.github.com>
2022-09-18 19:17:48 -0700

    Initial Commit

---
+++ MultiplicationTable/MultiplicationTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTableApp.swift
+//  MultiplicationTable
+//
+//  Created by OAA on 18/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
